2f324b9a017fbab536377e5aea0aa21d98cb8ed6
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-27 22:36:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre 
-
+ def initialize(title, author, page, genre)
+   @title = title
+   @author = author
+   @page_count = page 
+   @genre = genre 
 
 end 
 
